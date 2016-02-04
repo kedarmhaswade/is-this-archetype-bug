54e5c0f26ea29d5f035d9ab3e7c266a6769ad088
Kedar Mhaswade <kedar.mhaswade@gmail.com>
2016-02-04 13:35:11 -0800

    demo archetype bug?

---
+++ src/main/resources/archetype-resources/src/main/java/main/__mainRunner__.java
@@ -0,0 +1,14 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.main;
+// library classes
+import java.io.*;
+import java.util.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+/**
+ * The main class that runs the ${appName}. 
+ */
+public class ${mainRunner} {
+}
---
+++ src/main/resources/archetype-resources/src/test/java/main/__mainRunner__Test.java
@@ -0,0 +1,21 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.main;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+/**
+ * The test class that should contain the unit tests for __mainRunner__ class.
+ */
+public class ${mainRunner}Test {
+    /**
+     * Tests rigorously!
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue(true);
+    }
+}
